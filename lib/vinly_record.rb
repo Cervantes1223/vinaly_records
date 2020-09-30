72039cf171651828e6d985f4142e39fcda69b95a
Bryan Cervantes <bryanandkyle@gmail.com>
2020-09-30 18:10:50 +0000

    add desc

---
+++ lib/vinly_record.rb
@@ -0,0 +1,6 @@
+require "vinly_record/version"
+
+module VinlyRecord
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/vinly_record/version.rb
@@ -0,0 +1,3 @@
+module VinlyRecord
+  VERSION = "0.1.0"
+end
